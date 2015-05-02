e576f2da67c44c2cfe87f758498f434941aa358d
William High <whigh@netflix.com>
2015-05-02 08:21:25 -0700

    parallel frugal quantile

--- bin/online_quantile_test.sh
+++ bin/online_quantile_test.sh
@@ -3,8 +3,29 @@
 #http://arxiv.org/pdf/1407.1121v1.pdf
 
 n_data=1000000
-precision=1e4
-quantile=0.3
+precision=1e3
+
+function frugal_q () {
+    precision=$1
+    quantile=$2
+    gawk -v precision=$precision -v quantile=$quantile -v seed=$RANDOM '
+    BEGIN {
+        srand(seed)
+    }
+    {
+        r=rand()
+        $1=$1*precision
+        if ($1 > m && r > 1-quantile) {
+            m++
+        } else if ($1 < m && r < 1-quantile) {
+            m--
+        }
+    }
+    END {
+        print quantile,m/precision
+    }' quantile_data.txt ;
+}
+export -f frugal_q
 
 # make fake data
 if [ ! -f quantile_data.txt ]; then
@@ -27,24 +48,16 @@ gawk -v precision=$precision '
     }
 }
 END {
-    print m/precision
+    print 0.5,m/precision
 }' quantile_data.txt
 
 # frugal quantile
-# NOTE: THERE'S A TYPO IN THE FRUGAL QUANTILE ARXIV PAPER
-gawk -v precision=$precision -v quantile=$quantile -v seed=$RANDOM '
-BEGiN {
-    srand(seed)
-}
-{
-    r=rand()
-    $1=$1*precision
-    if ($1 > m && r > 1-quantile) {
-        m++
-    } else if ($1 < m && r < 1-quantile) {
-        m--
-    }
-}
-END {
-    print m/precision
-}' quantile_data.txt
+parallel -N1 frugal_q $precision ::: 0.01 0.05 0.1 0.5 0.9 0.95 0.99
+
+
+
+
+
+
+
+
