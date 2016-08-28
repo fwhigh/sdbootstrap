10450c7fecaa6e9ce534c5e80d450ada73933601
F. William High <fwhigh@gmail.com>
2016-08-28 14:02:32 -0700

    renaming package

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'Online distributed bootstrap',
+    'description': 'Streaming Distributed Bootstrap',
     'author': 'William High',
     'url': 'https://github.com/fwhigh/sdbootstrap',
     'download_url': 'https://github.com/fwhigh/sdbootstrap',
     'author_email': 'fwhigh@gmail.com',
     'version': '0.1',
     'install_requires': ['nose','scikit-learn','argparse','matplotlib','scipy','numpy'],
-    'packages': ['oblb'],
+    'packages': ['sdbootstrap'],
     'scripts': [],
-    'name': 'oblb'
+    'name': 'sdbootstrap'
 }
 
 setup(**config)
